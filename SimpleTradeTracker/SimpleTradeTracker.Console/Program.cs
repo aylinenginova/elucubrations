63d56b79c93f9753e2c6fe53fbbaafb238b2e649
Murat Bayan <murat.bayan@gmail.com>
2016-05-31 14:55:26 +0100

    Added a couple of static methods to test Git commits

--- SimpleTradeTracker/SimpleTradeTracker.Console/Program.cs
+++ SimpleTradeTracker/SimpleTradeTracker.Console/Program.cs
@@ -14,23 +14,22 @@ namespace SimpleTradeTracker.Console
         {
             using (var db = new DatabaseContext())
             {
-
-                var traders = db.Trader.Select(t => t).ToList();
-                var products = db.Product.Select(t => t).ToList();
+                //// Create a new Trade 
+                //var traders = db.Trader.Select(t => t).ToList();
+                //var products = db.Product.Select(t => t).ToList();
 
                 string selectedProductName = "Mdma";
                 string selectedTraderName = "Murat Bayan";
 
-                var trade = new Trade { Price = 10.3, Quantity = 4, Product = products.FirstOrDefault(e => e.Name == selectedProductName), Trader = traders.FirstOrDefault(e => e.Name == selectedTraderName), type = TradeType.Buy };
-                db.Trade.Add(trade);
+                CreateTrade(db, selectedTraderName, selectedProductName, 10.3, 10, TradeType.Sell);
 
-                db.SaveChanges();
-                //var traders = from b in db.Trader
-                //            orderby b.Name
-                //            select b;
-                //var product = from b in db.Trader
-                //            orderby b.Name
-                //            select b;
+                //var trade = new Trade { Price = 10.3, Quantity = 4, Product = products.FirstOrDefault(e => e.Name == selectedProductName), Trader = traders.FirstOrDefault(e => e.Name == selectedTraderName), type = TradeType.Buy };
+                //db.Trade.Add(trade);
+
+                //db.SaveChanges();
+
+
+
                 // Create and save a new Trader 
                 //System.Console.Write("Enter a name for a new trader: ");
                 //var name = System.Console.ReadLine();
@@ -60,11 +59,16 @@ namespace SimpleTradeTracker.Console
                 //    var product = new Product { Name = productName };
                 //    db.Product.Add(product);
                 //}
+
                 //var trader = new Trader { Name = name };
                 //db.Trader.Add(trader);
                 //db.SaveChanges();
 
+                string traderName = "John Doe";
 
+                var trader = CreateTrader(db, traderName);
+
+                System.Console.WriteLine($"Selected Trader: {trader.Name}");
 
                 // Display all Blogs from the database 
                 //var query = from b in db.Trader
@@ -84,6 +88,40 @@ namespace SimpleTradeTracker.Console
                 System.Console.WriteLine("Press any key to exit...");
                 System.Console.ReadKey();
             }
+        }
+
+        static void CreateTrade(
+            DatabaseContext context, 
+            string traderName,
+            string productName,
+            double price,
+            int quantity,   
+            TradeType tradeType)
+        {
+            var traders = context.Trader.Select(t => t).ToList();
+            var products = context.Product.Select(t => t).ToList();
+
+            var trade = new Trade { Price = price, Quantity = quantity, Product = products.FirstOrDefault(e => e.Name == productName), Trader = traders.FirstOrDefault(e => e.Name == traderName), type = tradeType };
+            context.Trade.Add(trade);
+            context.SaveChanges();
+        }
+
+        static Trader CreateTrader(DatabaseContext context, string traderName)
+        {
+            Trader trader;
+
+            if (!context.Trader.Any(t => t.Name == traderName))
+            {
+                trader = new Trader { Name = traderName };
+                context.Trader.Add(trader);
+                context.SaveChanges();
+            }
+            else
+            {
+                trader = context.Trader.Where(t => t.Name == traderName).First();
+            }
+
+            return trader;
         }
     }
 }
