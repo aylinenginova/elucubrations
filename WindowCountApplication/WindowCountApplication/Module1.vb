55c7914219b4d1f506a9c2b10581b3fd2e73796e
Murat Bayan <murat.bayan@gmail.com>
2016-08-30 21:51:31 +0100

    Added ExtendedWindowStyles data to filter out hidden windows Code refactor

--- WindowCountApplication/WindowCountApplication/Module1.vb
+++ WindowCountApplication/WindowCountApplication/Module1.vb
@@ -4,10 +4,13 @@
         Dim windowList As ArrayList
         windowList = New ArrayList()
         getWindowList(windowList)
-        For Each windowData As String In windowList
-            Console.WriteLine(windowData)
-        Next
+        'For Each windowData As String In windowList
+        '    Console.WriteLine(windowData)
+        'Next
 
+        For Each windowData As WindowDetails In windowList
+            Console.WriteLine(windowData.ToString())
+        Next
         Console.WriteLine("total count: " + windowList.Count().ToString())
         Console.ReadLine()
 
--- WindowCountApplication/WindowCountApplication/modEnumWindows.vb
+++ WindowCountApplication/WindowCountApplication/modEnumWindows.vb
@@ -1,15 +1,47 @@
 ﻿Imports System.Runtime.InteropServices
 Imports System.Text
 
-    Module modEnumWindows
+Module modEnumWindows
 
-        Private windowList As New ArrayList
-        Private errMessage As String
+    Public Structure RECT
+        Public Left As Integer
+        Public Top As Integer
+        Public Right As Integer
+        Public Bottom As Integer
+    End Structure
 
-        Public Delegate Function MyDelegateCallBack(ByVal hwnd As Integer, ByVal lParam As Integer) As Boolean
-        Declare Function EnumWindows Lib "user32" (ByVal x As MyDelegateCallBack, ByVal y As Integer) As Integer
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure WINDOWINFO
+        Public cbSize As UInteger
+        Public rcWindow As RECT
+        Public rcClient As RECT
+        Public dwStyle As UInteger
+        Public dwExStyle As UInteger
+        Public dwWindowStatus As UInteger
+        Public cxWindowBorders As UInteger
+        Public cyWindowBorders As UInteger
+        Public atomWindowType As UShort
+        Public wCreatorVersion As UShort
+    End Structure
 
-        Declare Auto Function GetClassName Lib "user32" _
+    Public Structure WindowDetails
+        Public ExtendedWindowStyle As UInteger
+        Public WindowClass As StringBuilder
+        Public WindowHandle As Integer
+        Public WindowTitle As StringBuilder
+        Public ProcessId As Integer
+        Public Overrides Function ToString() As String
+            Return String.Format("{0} - {1}" & vbCrLf & vbTab & vbTab & "{2} - hwnd {3}" & vbCrLf & vbTab & vbTab & "dwExStyle {4}" & vbCrLf, ProcessId, WindowTitle.ToString(), WindowClass.ToString(), WindowHandle, Hex(ExtendedWindowStyle))
+        End Function
+    End Structure
+
+    Private windowList As New ArrayList
+    Private errMessage As String
+
+    Public Delegate Function MyDelegateCallBack(ByVal hwnd As Integer, ByVal lParam As Integer) As Boolean
+    Declare Function EnumWindows Lib "user32" (ByVal x As MyDelegateCallBack, ByVal y As Integer) As Integer
+
+    Declare Auto Function GetClassName Lib "user32" _
         (ByVal hWnd As IntPtr,
         ByVal lpClassName As System.Text.StringBuilder,
         ByVal nMaxCount As Integer) As Integer
@@ -26,55 +58,62 @@ Imports System.Text
     Declare Auto Function IsWindowVisible Lib "user32" _
        (ByVal hWnd As IntPtr) As Boolean
 
+    Declare Auto Function GetWindowInfo Lib "user32" _
+       (ByVal hWnd As IntPtr,
+       ByRef pWindowInfo As WINDOWINFO) As Integer
+
     Private Function EnumWindowProc(ByVal hwnd As Integer, ByVal lParam As Integer) As Boolean
 
-            'working vars
-            Dim sTitle As New StringBuilder(255)
+        'working vars
+        Dim sTitle As New StringBuilder(255)
         Dim sClass As New StringBuilder(255)
         Dim processId As Integer
+        Dim windowInfo As WINDOWINFO
 
         Try
-
             Call GetClassName(hwnd, sClass, 255)
             Call GetWindowText(hwnd, sTitle, 255)
             Call GetWindowThreadProcessId(hwnd, processId)
-            'If IsWindowVisible(hwnd) Then
-            If sClass.ToString() = "IEFrame" Or sClass.ToString() = "Internet Explorer_Hidden" Then
-                'If sTitle.Length() > 0 Then
+            Call GetWindowInfo(hwnd, windowInfo)
 
-                windowList.Add(sClass.ToString & ", " & hwnd & ", " & sTitle.ToString & " - " & Process.GetProcessById(processId).ToString() & " - " & processId.ToString())
-                'End If
+            If IsWindowVisible(hwnd) Then
+                'If sClass.ToString() = "IEFrame" Or sClass.ToString() = "Internet Explorer_Hidden" Then
+                'If sTitle.Length() > 0 Then
+                If Not ((windowInfo.dwExStyle And &H80) = &H80) And Not ((windowInfo.dwExStyle And &H200000) = &H200000) Then
+                    'windowList.Add(Hex(windowInfo.dwExStyle) & " | " & sClass.ToString & ", " & hwnd & ", " & sTitle.ToString & " - " & Process.GetProcessById(processId).ToString() & " - " & processId.ToString())
+                    windowList.Add(New WindowDetails With {.WindowClass = sClass, .WindowHandle = hwnd, .WindowTitle = sTitle, .ExtendedWindowStyle = windowInfo.dwExStyle, .ProcessId = processId})
+                End If
             End If
 
 
         Catch ex As Exception
-                errMessage = ex.Message
-                EnumWindowProc = False
-                Exit Function
-            End Try
+            errMessage = ex.Message
+            EnumWindowProc = False
+            Exit Function
+        End Try
 
-            EnumWindowProc = True
+        EnumWindowProc = True
 
-        End Function
+    End Function
 
-        Public Function getWindowList(ByRef wList As ArrayList, Optional errorMessage As String = "") As Boolean
+    Public Function getWindowList(ByRef wList As ArrayList, Optional errorMessage As String = "") As Boolean
 
-            windowList.Clear()
+        windowList.Clear()
 
-            Try
-                Dim del As MyDelegateCallBack
-                del = New MyDelegateCallBack(AddressOf EnumWindowProc)
-                EnumWindows(del, 0)
-                getWindowList = True
-            Catch ex As Exception
-                getWindowList = False
-                errorMessage = errMessage
-                Exit Function
-            End Try
+        Try
+            Dim del As MyDelegateCallBack
+            del = New MyDelegateCallBack(AddressOf EnumWindowProc)
+            EnumWindows(del, 0)
+            getWindowList = True
+        Catch ex As Exception
+            getWindowList = False
+            errorMessage = errMessage
+            Exit Function
+        End Try
 
-            wList.Clear()
-            wList = windowList
+        wList.Clear()
+        wList = windowList
 
-        End Function
+    End Function
 
-    End Module
+End Module
